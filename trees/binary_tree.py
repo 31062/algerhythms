7ae19a78952a089e7d9e96a2fb31c525eae8257a
robert cook <31062@longroad.ac.uk>
2014-10-06 16:06:59 +0100

    first binary trees work

---
+++ trees/binary_tree.py
@@ -0,0 +1,32 @@
+class Binary_tree:
+    """inplmenting a biranry search"""
+    def __init__(self):
+        self.root = None
+        self.left = None
+        self.right = None
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def set_root_val(self,val):
+        self.root = val
+
+    def get_root_val(self):
+        return self.root
+
+    def insert_left(self,val):
+        self.left = 
+
+    def insert_right(self,val):
+        self.right = val
+
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    a = Binary_tree
+    
