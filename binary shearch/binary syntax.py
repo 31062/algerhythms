12aa4e049a3bd91956fb38c5754c3f10fb80d83a
robert cook <31062@longroad.ac.uk>
2014-10-13 16:07:19 +0100

    birnary shearch

---
+++ binary shearch/binary syntax.py
@@ -0,0 +1,16 @@
+list_ = [5,7,1,2,6,4,9,8,10,0,3]
+def binary_shearch(list, first, last, item_sought):
+    item_found = False
+    search_failed = False
+    item_sought = int(input("please enter a number between 0,10")
+    while item_found not True:
+        first = 0
+        last = len(list_)
+        midpoint = (first + last) // 2
+        if midpoint ==
+
+def main():
+
+                      
+if __name__ == "__main__":
+    def main()
